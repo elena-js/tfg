f15bbe013f4508d4909173804a2a9fb983d72fd9
elena-js <83721658+elena-js@users.noreply.github.com>
2022-06-08 19:04:54 +0200

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -4,35 +4,35 @@
 import json
 import re
 import emoji
+#import spacy
 
 import pandas as pd
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.sentiment.util import *
 
-# función extraer emojis
-def extract_emojis(str):
-    return ''.join(c for c in str if c in emoji.UNICODE_EMOJI['en'])
-
 # obtencion del array de comentarios a partir del json
-f = open("comments_k.json", "r")
+f = open("comments.json", "r")
 content = f.read()
 info_com = json.loads(content)
 
-# obtencion texto comentarios y usuarios
+# obtencion texto comentarios, usuarios, likes
 comments_in = []
 users = []
+likes = []
 for item in info_com:
     comments_in.append(item['text'])
     users.append(item['owner']['username'])
+    likes.append(item['likes_count'])
     if len(item['answers']) != 0:
         for i in item['answers']:
             comments_in.append(i['text'])
             users.append(i['owner']['username'])
+            likes.append(item['likes_count'])
 
 # variables del preprocess
 wordlist = [] # array bow
@@ -47,16 +47,15 @@ for comment in comments_in:
     
     # quitar emojis
     em = ''
-    em2 = []
-    e = extract_emojis(comment)
-    for s in e:
+    em_array = []
+    comment_em = ''.join(c for c in comment if c in emoji.UNICODE_EMOJI['en'])
+    for e in comment_em:
         #if s not in em:
-        em2.append(s)
-        em += s
-        #em = em + s + ' '
+        em_array.append(e)
+        em += e
     if not em:
         em = ' '
-    comment = ''.join( x for x in comment if x not in em2)
+    comment = ''.join( x for x in comment if x not in em_array)
 
     # quitar URLs
     urlPattern = r"((http://)[^ ]*|(https://)[^ ]*|( www\.)[^ ]*)"
@@ -65,33 +64,47 @@ for comment in comments_in:
     # hacer lista de tokens
     tokens = word_tokenize(comment)
 
-    # quitar menciones
     #mention = []
     mention = ''
     for i in range(len(tokens)):
+
+        # quitar menciones
         if tokens[i] == '@' and i < (len(tokens) - 1):
             #mention.append(tokens[i+1])
             mention = mention + tokens[i+1] + ' '
             tokens[i] = ''
             tokens[i+1] = ''
+        
+        # quedarse con negación
+        if tokens[i] == 'n\'t':
+            tokens[i] = 'not'
+        if tokens[i] == 'haven' or tokens[i] == 'didn' or tokens[i] == 'wouldn' or tokens[i] == 'mightn' or tokens[i] == 'mustn' or tokens[i] == 'weren' or tokens[i] == 'hadn' or tokens[i] == 'shouldn' or tokens[i] == 'isn' or tokens[i] == 'wasn' or tokens[i] == 'doesn' or tokens[i] == 'couldn' or tokens[i] == 'hasn' or tokens[i] == 'shan' or tokens[i] == 'needn' or tokens[i] == 'aren' or tokens[i] == 'won' or tokens[i] == 'don' :
+            tokens[i] = 'not'
 
         # limpiar texto
-        TEXT_CLEANING_RE = "[^a-zA-Z0]+"
+        TEXT_CLEANING_RE = "[^a-zA-Z]+"
         #TEXT_CLEANING_RE = "[^a-zA-Z0-9]+"  -- con números
         tokens[i] = re.sub(TEXT_CLEANING_RE, '', tokens[i].lower())
     
     # quitar stopwords
     stops = set(stopwords.words('english'))
-    tokens = [t for t in tokens if t not in stops]
+    stops.remove('not')
+    stops.remove('no')
 
+    tokens = [t for t in tokens if t not in stops]
+    
     # stemming/lemmatization
     stemmer = PorterStemmer()
+    lemmatizer = WordNetLemmatizer()
     bow = []
+    '''
     for t in tokens:
         bow.append(stemmer.stem(t))
-    
+        #bow.append(lemmatizer.lemmatize(t))
+    '''
     # juntar palabras y volver a tokenizar para quitar espacios
-    comment = (' '.join(bow))
+    #comment = (' '.join(bow))
+    comment = (' '.join(tokens))
     bow = word_tokenize(comment)
     n_words.append(len(bow))
     comment = (' '.join(bow))
@@ -130,15 +143,14 @@ sent = []
 sent_num = []
 for c in range(len(cl_comments)):
     scores = s_an.polarity_scores(cl_comments[c])
-    #sent.append(scores['compound'])
-    '''
-    if scores['compound'] >= 0.5:
+    # sin tener en cuenta emojis:
+    if scores['compound'] >= 0.4:
         sent.append("pos")
-    elif scores['compound'] <= -0.5:
+    elif scores['compound'] <= -0.4:
         sent.append("neg")
     else:
         sent.append("neu")
-    '''
+    
     if (n_emojis[c] + n_words[c]) != 0:
         fw = n_words[c] / (n_emojis[c] + n_words[c])
         fe = n_emojis[c] / (n_emojis[c] + n_words[c])
@@ -146,19 +158,23 @@ for c in range(len(cl_comments)):
     else:
         s = 0
     sent_num.append(s)
+    
+    # teniendo en cuenta emojis:
+    '''
     if s >= 0.5:
         sent.append("pos")
     elif s <= -0.5:
         sent.append("neg")
     else:
         sent.append("neu")
+    '''
 
 # HACER CSV
 import pandas as pd
 
-dict = {'comment': comments_in, 'cl_comment': cl_comments, 'wordlist': wordlist, 'emojis': emojis, 'sent_num': sent_num, 'sent': sent, 'user': users, 'mentioned': mentions} 
+dict = {'comment': comments_in, 'cl_comment': cl_comments, 'wordlist': wordlist, 'emojis': emojis, 'sent_num': sent_num, 'sent': sent, 'user': users, 'mentioned': mentions, 'likes': likes} 
 df = pd.DataFrame(dict) 
-df.to_csv('datap.csv')
+df.to_csv('data.csv')
 #df.to_csv('data.csv', sep=';')
 print('\ndataset creado correctamente')
 
--- sent_an.py
+++ sent_an.py
@@ -5,7 +5,6 @@ from nltk.sentiment.util import *
 import pandas as pd	
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
 import joblib
 
 # obtencion del dataset
@@ -18,13 +17,21 @@ x_train0, x_test0, y_train, y_test = train_test_split(data, data['sent'], test_s
 x_train0.to_csv('train.csv') # ------------------------ ES NECESARIO?
 x_test0.to_csv('test.csv')
 
+num_comments = x_test0.shape[0] # número de comentarios del test
+
 # VECTORIZACIÓN - COUNTVECTORIZER / TFIDFVECTORIZER
-vectorizer1 = CountVectorizer()
+vectorizer1 = CountVectorizer() # ngram_range=(1,2)
 vectorizer2 = TfidfVectorizer()
 #vectorizer = TfidfVectorizer(min_df=5, max_df=0.8, sublinear_tf=True, use_idf=True)
 
-train_vectors = vectorizer1.fit_transform(x_train0['wordlist'].values.astype('U'))
-test_vectors = vectorizer1.transform(x_test0['wordlist'].values.astype('U'))
+# metiendo el comentario entero
+train_vectors = vectorizer1.fit_transform(x_train0['cl_comment'].values.astype('U')).toarray()
+test_vectors = vectorizer1.transform(x_test0['cl_comment'].values.astype('U')).toarray()
+
+# metiendo la lista de tokens
+#train_vectors = vectorizer1.fit_transform(x_train0['wordlist'].values.astype('U'))
+#test_vectors = vectorizer1.transform(x_test0['wordlist'].values.astype('U'))
+print(vectorizer1.get_feature_names_out())
 
 # PARA METER AL SISTEMA X SERÁN COMENTARIOS (VECTORS), Y SERÁN LOS VALORES DE SENTIMIENTO
 
@@ -34,7 +41,12 @@ x_test = test_vectors
 # CLASIFICADOR SVM ----------------------------------------------------------------------------------
 
 # definir algoritmo
+from sklearn.svm import SVC
 algoritmo = SVC(kernel='linear')
+
+# Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+#algoritmo = GaussianNB()
 
 # entrenar modelo
 algoritmo.fit(x_train, y_train)
@@ -61,7 +73,7 @@ print(report)
 
 # obtener + datos (ej:user)
 neg_users = []
-d = []
+likes = []
 i = 0
 while i < len(y_pred):
     if y_pred[i]== 'neg':
@@ -69,6 +81,8 @@ while i < len(y_pred):
         print('\nComentario negativo:\n' + c['comment'])
         if c['user'] not in neg_users:
             neg_users.append([c['user'], 1])
+            likes.append(c['likes'])
+            print('likes:' + str(c['likes']))
         else:
             for user in neg_users:
                 if user[0] == c['user']:
@@ -77,8 +91,23 @@ while i < len(y_pred):
 print('\nUsuarios que han realizado algún comentario negativo:')
 print(neg_users)
 
+# ver numero de comentarios negativos
+num_neg = len(likes)
+print('\nComentarios negativos: ' + str(num_neg))
+p_neg = num_neg / num_comments
+print('\n% comentarios negativos: ' + str(p_neg*100))
+
+'''
+if p_neg < 0.2:
+    print('\nProbabilidad baja de ciberacoso')
+elif 0.2 < p_neg < 0.4:
+    print('\nProbabilidad media de ciberacoso')
+elif p_neg > 0.4:
+    print('\nProbabilidad alta de ciberacoso')
+''' 
+
 # guardar modelo
-joblib.dump(algoritmo, 'svm_entrenado.pkl')
+#joblib.dump(algoritmo, 'svm_entrenado.pkl')
 print('\nModelo guardado')
 
 #------------------------------------------------------------------------------------------------------
